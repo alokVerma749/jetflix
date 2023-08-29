75aa6904f22e521f90a453e41f9e09618fbc8528
alok verma <alokverma749@gmail.com>
2023-08-29 08:31:53 +0530

    created some components

---
+++ src/components/Login.js
@@ -0,0 +1,12 @@
+const Login = () => {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="email/phoneNumber"></label>
+                <input type="text" id="email/phoneNumber" placeholder="" />
+            </form>
+        </div>
+    )
+}
+
+export default Login;
